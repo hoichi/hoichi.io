f33c55e3d6ecc7596942d3106be077a7e1ad9cd7
hoichi <samokhov@gmail.com>
2015-10-29 00:58:39 +0300

    Backup

--- modules/page.js
+++ modules/page.js
@@ -1,50 +1,129 @@
-/**
- * Created by hoichi on 18.10.15.
- */
-
 'use strict';
+
+const   fm = require('front-matter'),
+        fs = require('fs'),
+        path = require('path'),
+        stampit = require('stampit');
 
 /*
  *  Takes a source object with a source path etc. And the options from the global config.
- *  TODO: proxies!  https://ponyfoo.com/articles/es6-proxies-in-depth
- *  TODO: an interface that _p should conform to
  */
-function PageFactory(pageSource, options) {
-    var _p = {
-        index,
-        category,       // post category
-        content,        // post content in HTML (HTML is lingua franca and we're agnostic
-                            // of original markup at this point)
-        description,    // post description for meta, search engines and prob. TOCs and archive stuff
-        excerpt,        // before the fold
-        title,
-        site,           // the object for the site that contains the page
-        slug,           // the part of the URL after the last slash
-        source,         // an object dealing with the MD source (do we need to expose it in API?)
-        style,          // looks specific to the page
-        tags            // a bunch of tags
-    };
+function Page(source, options) {
+    var _isReady = false,
+        _category,      // post category
+        _content,       // post content in HTML (HTML is lingua franca and we're agnostic
+                            // of original markup as much as possible)
+        _description,   // post description for meta, search engines and prob. TOCs and archive stuff
+        _excerpt,       // before the fold
+        _isStub = false,
+        _title,
+        _site,          // the object for the site that contains the page
+        _slug,          // the part of the URL after the last slash
+        _source,        // an object dealing with the MD source (do we need to expose it in API?)
+        _style,         // looks specific to the page
+        _tags;          // a bunch of post tags
 
-    options = Object.assign({
-        markup: s => s  // do nufin' by default. agnostic, muthafucka!
-    }, options);
+    function ifReadyCb(cb) {
+        if (!_isReady) {
+            throw new Error('Object Page should be properly filled before you can consume it\'s data. Use `fromSource` or something.');
+        } else {
+            return cb();
+        }
+    }
 
-    return init(pageSource, init)
+    function ifReady(val) {
+        if (!_isReady) {
+            throw new Error('Object Page should be properly filled before you can consume it\'s data. Use `fromSource` or something.');
+        } else {
+            return val;
+        }
+    }
+
+    function ready(val) {
+        if (typeof val !== 'undefined' && typeof val !== 'null') {
+            _isReady = !!val;
+        }
+
+        return val;
+    }
+
+    function fromSource(source, options) {
+        // fixme: нужны ли дефолты здесь?
+        const cfg = Object.assign({
+            defCat: 'blog',
+            extensions: '*',
+            encoding: 'UTF-8',
+            markup: s => s,  /* do nufin' by default. agnostic, muthafucka! */
+            rootDir: __dirname
+        }, options);
+
+        const baseParser = new RegExp(`(.*)\.(${cfg.extensions})`);
+
+        var rel = path.relative(cfg.rootDir, source.path),
+            base = path.basename(rel).match(baseParser)[1],
+            dir = path.dirname(rel),
+            srcCont = source.contents.toString(cfg.encoding),
+            meta,
+            body;
+
+        _source = source;
+
+        try {
+            ({attributes: meta, body} = fm(srcCont));
+        } catch(Err) {
+            throw new Error(`Reading front-matter failed (${Err.message}`);
+        }
+
+        try {
+            _content = cfg.markup(body);
+        } catch(Err) {
+            throw new Error("Markup conversion failed:\n" + Err.message);
+        }
+
+        _category       = meta.category     || cfg.defCat;
+        _excerpt        = meta.excerpt      || '';          // todo: first paragraph
+        _description    = meta.description  || _excerpt;
+        _title          = meta.title        || '';
+        _tags           = meta.tags         || [];
+
+        _isStub = (meta.isStub || !_title || !_content);
+        if (_isStub) {
+            // not sure if it actually frees any memory immediately but let's try
+            _content = _description = _excerpt = '';
+        }
+
+        _slug = slugify(_title);
+    }
 
     return {
-        get index() {return _p.index},
-        get category() {return _p.category},
-        get content() {return _p.content},
-        get description() {return _p.description},
-        get excerpt() {return _p.excerpt},
-        get title() {return _p.title},
-        get slug() {return _p.slug},
-        get tags() {return _p.tags},
+        get category()      {return ifReady(_category)},
+        get content()       {return ifReady(_content)},
+        get description()   {return ifReady(_description)},
+        get excerpt()       {return ifReady(_excerpt)},
+        get title()         {return ifReady(_title)},
+        get slug()          {return ifReady(_slug)},
+        get tags()          {return ifReady(_tags)},
 
         url() {return `${_p.category}/${_p.slug}`;    /*todo*/ },
 
         prev() {return _p.site.prev(_p.index)},
 
-        next() {return _p.site.next(_p.index)}
+        next() {return _p.site.next(_p.index)},
+
+        ready,
+        fromSource
+    };
+
+    function slugify(s) {
+        return s.replace(/\s|\W/, '-');
     }
+}
+
+function makeAPath(meta, template) {
+    var re = /\$\((\w+)\)/g;
+
+    return template.replace(
+        re,
+        (_match, key) => {meta[key]}
+    );
 }
