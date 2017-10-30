0193d665c80519d168deb70e88511d76f40c333c
hoichi <hi@hoichi.io>
2017-10-30 07:51:08 +0300

    Removing the built scripts from the repo

--- scripts/model/page.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/*
- * Takes a path (and a working dir)
- * returns an object with:
- * - file base (sans extension)
- * - an arr of parent dirs
- * */
-//# sourceMappingURL=page.js.map
