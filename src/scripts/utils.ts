274ecd1c64c3825c93e8566b073735bc3099dd34
Sergio Samokhov <hi@hoichi.io>
2018-05-23 11:09:39 +0300

    Dead code removal
    Shake that pig out of the bush!

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -3,55 +3,7 @@
  */
 'use strict';
 
-import * as fs from 'fs';
 import * as path from 'path';
-
-function slugify(s) {
-  if (typeof s !== 'string') {
-    throw new Error("You cannot slugify it if it's not a string.");
-  } else if (!s) {
-    throw new Error(
-      "What's your business passing an empty string? No such thing as an empty slug.",
-    );
-  } else {
-    return encodeURI(s.toLowerCase().replace(/\s|\W/g, '-'));
-  }
-}
-
-function tplToPath(meta, template) {
-  const re = /\$\((\w+)\)/g;
-
-  return template.replace(re, (_match, key) => meta[key]);
-}
-
-function makePathSync(sPath, { baseDir = null } = {}) {
-  const p = path.parse(sPath);
-  let dir = baseDir || p.root;
-
-  // $TODO:   throw on empty baseDir
-  //          (or require full paths, for that matter)
-
-  p.dir.split(path.sep).forEach((el, idx, arr) => {
-    dir = path.resolve(dir, el);
-    try {
-      fs.accessSync(dir, fs.constants.F_OK);
-    } catch (err) {
-      if (err.code !== 'ENOENT') {
-        throw err;
-      }
-
-      fs.mkdirSync(dir);
-    }
-  });
-}
-
-function renderTemplate(tpl, data, dirs) {
-  const html = tpl(data),
-    sPath = Array.isArray(dirs) ? path.join(...dirs) : dirs;
-
-  makePathSync(sPath);
-  fs.writeFileSync(sPath, html, { flag: 'w+' });
-}
 
 function dateFormatter(locale = 'en_US', options) {
   // $TODO: something more customizable? moments.js? or?
@@ -91,10 +43,6 @@ function constructPageUrl(page) {
 export {
   dateFormatter,
   extract1stHtmlParagraph,
-  makePathSync,
-  renderTemplate,
-  slugify,
-  tplToPath,
   constructPageUrl,
   l,
 };
