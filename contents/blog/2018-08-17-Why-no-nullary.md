9edf9e772b184be063f4b92985ad010ddb5c9593
Sergey Samokhov <s.samokhov@aventica.ru>
2018-08-17 12:31:28 +0300

    post: why-no-nullary

---
+++ contents/blog/2018-08-17-Why-no-nullary.md
@@ -0,0 +1,11 @@
+---
+permalink: false
+title: "TIL: Why ReasonML Has No Nullary Functions"
+date: 2017-08-17
+slug: why-no-nullary
+excerpt: If you don’t provide all of a function’s parameters, you get a new function from the remaining parameters to the result. As a consequence, if you could actually provide no parameters at all, then func() would be the same as func and neither would actually call func.
+tags: [ TIL, Reason, OCaml, Dr Axel]
+---
+
+> Why doesn’t ReasonML have nullary functions? That is due to ReasonML always performing partial application (explained in detail later): If you don’t provide all of a function’s parameters, you get a new function from the remaining parameters to the result. As a consequence, if you could actually provide no parameters at all, then func() would be the same as func and neither would actually call func.
+> -- [Exploring ReasonML and functional programming](http://reasonmlhub.com/exploring-reasonml/ch_functions.html#there-are-no-functions-without-parameters) by Dr. Axel Rauschmayer
--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -6,43 +6,44 @@
 import * as path from 'path';
 
 function dateFormatter(locale = 'en_US', options) {
-  // $TODO: something more customizable? moments.js? or?
-  let dtf = new Intl.DateTimeFormat(locale, options);
-  return dtf.format.bind(dtf);
+	// $TODO: something more customizable? moments.js? or?
+	let dtf = new Intl.DateTimeFormat(locale, options);
+	return dtf.format.bind(dtf);
 }
 
 function extract1stHtmlParagraph(html) {
-  let excerpt = /<p>(.*)?<\/p>/.exec(html)[1];
-  return excerpt.replace(/<(.|\n)*?>/g, '');
+	// prettier-ignore
+	let excerpt = (
+    /<p>(.*)?<\/p>/.exec(html)
+    || /<blockquote>(.*)?<\/blockquote>/.exec(html)
+    || [ , '...' ]
+  )[1];
+
+	return excerpt.replace(/<(.|\n)*?>/g, '');
 }
 
 function l(val, desc) {
-  if (desc) console.log(desc, val);
-  else console.log(val);
+	if (desc) console.log(desc, val);
+	else console.log(val);
 
-  return val;
+	return val;
 }
 
 function constructPageUrl(page) {
-  let url = page.url;
+	let url = page.url;
 
-  if (!url) {
-    let slug = page.slug;
-    if (slug == null) {
-      slug = (name => (name === 'index' ? '' : name))(page.path['name']);
-    }
+	if (!url) {
+		let slug = page.slug;
+		if (slug == null) {
+			slug = ((name) => (name === 'index' ? '' : name))(page.path['name']);
+		}
 
-    // fixme: don’t render urls with `path` methods
-    url = path.join(page.category || page.path['dir'], slug);
-  }
+		// fixme: don’t render urls with `path` methods
+		url = path.join(page.category || page.path['dir'], slug);
+	}
 
-  return url.replace(/\\/g, '/') || 'untitled/';
+	return url.replace(/\\/g, '/') || 'untitled/';
 }
 
 //noinspection SpellCheckingInspection
-export {
-  dateFormatter,
-  extract1stHtmlParagraph,
-  constructPageUrl,
-  l,
-};
+export { dateFormatter, extract1stHtmlParagraph, constructPageUrl, l };
